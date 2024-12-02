8ffe3d6b9ff2be669648cf5f26260ad4edd0e9f8
Paul Martins <vmartins.paul@gmail.com>
2024-12-02 21:24:57 +0000

    Added tests for the record variant of the nested class

---
+++ src/Fluentify.Console.Tests/Record/NestedTests/WhenWithAttributesIsCalled.cs
@@ -0,0 +1,41 @@
+﻿namespace Fluentify.Console.Record.NestedTests;
+
+public sealed class WhenWithAttributesIsCalled
+{
+    [Fact]
+    public void GivenNullSubjectThenArgumentNullExceptionIsThrown()
+    {
+        // Arrange
+        Nested.Simple? subject = default;
+
+        // Act
+        Func<Nested.Simple> act = () => subject!.WithAttributes(new object());
+
+        // Assert
+        _ = act.Should().Throw<ArgumentNullException>()
+            .WithParameterName(nameof(subject));
+    }
+
+    [Fact]
+    public void GivenAttributesThenTheValueIsApplied()
+    {
+        // Arrange
+        object[] attributes = [new()];
+
+        var original = new Nested.Simple
+        {
+            Age = Random.Shared.Next(),
+            Attributes = [],
+            Name = "Avery Brooks",
+        };
+
+        // Act
+        Nested.Simple actual = original.WithAttributes(attributes);
+
+        // Assert
+        _ = actual.Should().NotBeSameAs(original);
+        _ = actual.Age.Should().Be(original.Age);
+        _ = actual.Attributes.Should().BeEquivalentTo(attributes);
+        _ = actual.Name.Should().Be(original.Name);
+    }
+}
---
+++ src/Fluentify.Console.Tests/Record/NestedTests/WhenWithNameIsCalled.cs
@@ -0,0 +1,42 @@
+﻿namespace Fluentify.Console.Record.NestedTests;
+
+public sealed class WhenWithNameIsCalled
+{
+    [Fact]
+    public void GivenNullSubjectThenArgumentNullExceptionIsThrown()
+    {
+        // Arrange
+        Nested.Simple? subject = default;
+
+        // Act
+        Func<Nested.Simple> act = () => subject!.WithName("Avery Brooks");
+
+        // Assert
+        _ = act.Should().Throw<ArgumentNullException>()
+            .WithParameterName(nameof(subject));
+    }
+
+    [Theory]
+    [InlineData("Avery Brooks")]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void GivenANameThenTheValueIsApplied(string name)
+    {
+        // Arrange
+        var original = new Nested.Simple
+        {
+            Age = Random.Shared.Next(),
+            Attributes = [new(), new()],
+            Name = "Patrick Stewart",
+        };
+
+        // Act
+        Nested.Simple actual = original.WithName(name);
+
+        // Assert
+        _ = actual.Should().NotBeSameAs(original);
+        _ = actual.Age.Should().Be(original.Age);
+        _ = actual.Attributes.Should().BeEquivalentTo(original.Attributes);
+        _ = actual.Name.Should().BeEquivalentTo(name);
+    }
+}
