426e43449b7ecf8653c4d2ad198bb575b94baf67
Paul Martins <50200071+MooVC@users.noreply.github.com>
2026-02-14 19:32:33 +0000

    Add coverage for SkipAutoInitialization console samples

---
+++ src/Fluentify.Console.Tests/Record/SkipAutoInitializationOnPropertyTests/WhenWithDependencyIsCalled.cs
@@ -0,0 +1,47 @@
+namespace Fluentify.Console.Record.SkipAutoInitializationOnPropertyTests;
+
+public sealed class WhenWithDependencyIsCalled
+{
+    [Fact]
+    public void GivenNullDependencyWhenBuilderIsUsedThenNotSupportedExceptionIsThrown()
+    {
+        // Arrange
+        var subject = new SkipAutoInitializationOnProperty(Random.Shared.Next(), null!);
+
+        // Act
+        Action act = () => subject.WithDependency(instance =>
+        {
+            instance.Name = "Avery Brooks";
+            return instance;
+        });
+
+        // Assert
+        act.ShouldThrow<NotSupportedException>();
+    }
+
+    [Fact]
+    public void GivenDependencyWhenBuilderIsUsedThenTheValueIsApplied()
+    {
+        // Arrange
+        const string expected = "Avery Brooks";
+
+        var subject = new SkipAutoInitializationOnProperty(
+            Random.Shared.Next(),
+            new SkipAutoInitializationOnProperty.Dependent
+            {
+                Name = "Patrick Stewart",
+            });
+
+        // Act
+        SkipAutoInitializationOnProperty actual = subject.WithDependency(instance =>
+        {
+            instance.Name = expected;
+            return instance;
+        });
+
+        // Assert
+        actual.ShouldNotBeSameAs(subject);
+        actual.Age.ShouldBe(subject.Age);
+        actual.Dependency.Name.ShouldBe(expected);
+    }
+}
